ae4a8fd3c38252f2a2d457b1b19bdeb9e15ecd57
Jepkosgei3 <95687538+Jepkosgei3@users.noreply.github.com>
2025-07-24 16:42:10 +0300

    Update db.py

--- server/db.py
+++ server/db.py
@@ -2,6 +2,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://cosydeveloper:fMhrXwdk9H5PpC9p@bug-tracker-cluster.gmszx4m.mongodb.net/mstock?retryWrites=true&w=majority")
+MONGO_URL = (os.getenv("MONGO_URI"))
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["mstock"]
